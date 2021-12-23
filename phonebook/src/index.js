03b0b77afbfef7257bb1072136a264737e3c590e
Zixin Zhou <zixin.zhou_stella@outlook.com>
2021-12-23 18:35:18 +0200

    2.11

--- phonebook/src/index.js
+++ phonebook/src/index.js
@@ -1,7 +1,14 @@
 import ReactDOM from 'react-dom'
 import App from './App'
+import axios from 'axios'
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
-)
+axios.get('http://localhost:3001/persons').then(response => {
+  const data = response.data
+  ReactDOM.render(
+    <App notes={data}/>, 
+    document.getElementById('root')
+  )
+})
+
+
+
